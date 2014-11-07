6cea1acec4bc9f8a7eb55bd11c82e80dac8c5657
Vigneash Sundar <vigneashsundar@live.com>
2014-11-07 18:50:27 +0530

    working GDT

--- compile.sh
+++ compile.sh
@@ -2,7 +2,8 @@ nasm -f aout -o start.o boot_code.asm
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
 
-ld -T link.ld -o kernel.bin start.o main.o scrn.o
+ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o
 
 
